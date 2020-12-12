1639ae46c0d369721110d4a3a502e28f2adddc9f
Malex1982 <malex1982@mail.ru>
2020-12-12 15:49:39 +0300

    'ready'

--- Json_learn.py
+++ Json_learn.py
@@ -1,27 +1,19 @@
 import json
 with open('data_file.json') as file:
     info = json.loads(file.read())
-professions = [] #это делаю для того, чтобы вытянуть все профессии из файла
+prof = []
 for i in range(len(info)):
-    professions.append(info[i].get("profession"))
-professions = list(set(professions))
-print(professions) #конец блока, можно и без него
-for i in range(len(info)):
-    if info[i].get("profession") == 'engineer':
-        engineer.append(info[i])
-    elif info[i].get("profession") == 'director':
-        director.append(info[i])
-    elif info[i].get("profession") == 'manager':
-        manager.append(info[i])
-engineer_json = json.dumps(engineer, indent = 4)
-manager_json = json.dumps(manager, indent = 4)
-director_json = json.dumps(director, indent = 4)
-with open('manager.json', 'w') as file:
-    file.write(manager_json)
-with open('engineer.json', 'w') as file:
-    file.write(engineer_json)
-with open('director.json', 'w') as file:
-    file.write(director_json)
+    prof.append(info[i].get("profession"))
+prof = list(set(prof))
+for i in range(len(prof)):
+    s = prof[i]
+    m = []
+    for k in range(len(info)):
+        if info[k].get("profession") == prof[i]:
+            m.append(info[k])
+            j = json.dumps(m, indent = 4)
+            with open(f'{s}.json', 'w') as file:
+                file.write(j)
 
 
 
