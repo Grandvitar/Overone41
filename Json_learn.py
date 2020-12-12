6591ebf650f34f349752e2a7e7d47827e5207e18
Malex1982 <malex1982@mail.ru>
2020-12-12 14:23:51 +0300

    'списки'

--- Json_learn.py
+++ Json_learn.py
@@ -1,17 +1,30 @@
 import json
-
-dict = {'first_name':['Alex', 'Pavel', 'Igor', 'Petr'],
-        'last_name': ['Meleshko', 'Ivanov', 'Petrov', 'Sidorov'],
-        'family_name': ['Ivanovich', 'Sergeevich', 'Olegivich', 'Petrovich'],
-        'age': [38, 26, 38, 39, 36],
-        'spec': ['manager', 'manager', 'finansist', 'director']}
-incoding_json = json.dumps(dict, indent = 4, separators = ('', ' : '))
-print(incoding_json)
-with open('data_file.json', 'w') as file:
-    file.write(incoding_json)
 with open('data_file.json') as file:
     info = json.loads(file.read())
-print(info)
+professions = [] #это делаю для того, чтобы вытянуть все профессии из файла
+for i in range(len(info)):
+    professions.append(info[i].get("profession"))
+professions = list(set(professions))
+print(professions) #конец блока, можно и без него
+for i in range(len(info)):
+    if info[i].get("profession") == 'engineer':
+        engineer.append(info[i])
+    elif info[i].get("profession") == 'director':
+        director.append(info[i])
+    elif info[i].get("profession") == 'manager':
+        manager.append(info[i])
+engineer_json = json.dumps(engineer, indent = 4)
+manager_json = json.dumps(manager, indent = 4)
+director_json = json.dumps(director, indent = 4)
+with open('manager.json', 'w') as file:
+    file.write(manager_json)
+with open('engineer.json', 'w') as file:
+    file.write(engineer_json)
+with open('director.json', 'w') as file:
+    file.write(director_json)
+
+
+
 
 
 
